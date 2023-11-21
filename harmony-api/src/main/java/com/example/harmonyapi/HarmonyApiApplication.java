eab703c8c8ab78ff2a42d4f7c72b4425a9485aec
pauline <pauline.contat00@gmail.com>
2023-11-21 09:03:57 +0100

    Projet Spring de base + test connexion à db locale

---
+++ harmony-api/src/main/java/com/example/harmonyapi/HarmonyApiApplication.java
@@ -0,0 +1,29 @@
+package com.example.harmonyapi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootApplication
+public class HarmonyApiApplication implements CommandLineRunner {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonyApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String sql = "INSERT INTO public.user (identifiant, mdp, pseudo) VALUES ('id', 'mdp', 'pseudo')";
+
+		int rows = jdbcTemplate.update(sql);
+		if (rows > 0) {
+			System.out.println("A new row has been inserted.");
+		}
+	}
+
+}
---
+++ harmony-api/src/test/java/com/example/harmonyapi/HarmonyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.harmonyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
