7dbd1c5ccb6859701b655bd88af729cc31c8c4f3
pauline <pauline.contat00@gmail.com>
2024-01-19 09:14:38 +0100

    ajout image

---
+++ front-end3/front-end (copy)/src/components/AffichageImage.vue
@@ -0,0 +1,49 @@
+<template>
+    <div v-if="visible" class="modal is-active" id="myModal">
+        <div class="modal-background">
+        </div>
+        <div class="modal-card">
+            <header class="modal-card-head">
+                <p class="modal-card-title">Ticket de la dépense</p>
+                <button class="delete" aria-label="close" @click="fermerModal"></button>
+            </header>
+            <div class="modal-card-body">
+                <img v-if="depense.image" :src="depense.image" alt="Image de la dépense" />
+                <p v-else>Aucun ticket pour cette dépense</p>
+            </div>
+            <footer class="modal-card-foot">
+                <button class="button" @click="fermerModal">Fermer</button>
+            </footer>
+        </div>
+    </div>
+</template>
+
+<script>
+import Expense from '../models/expense.js';
+
+export default {
+    name: 'AffichageImage',
+    props: {
+        // depense: {
+        //     type: Expense,
+        //     required: true,
+        // },
+    },
+    data() {
+        return {
+            visible: false,
+            depense: null,
+        };
+    },
+    methods: {
+        ouvrirModal(depense) {
+            this.visible = true;
+            this.depense = depense;
+        },
+        fermerModal() {
+            this.visible = false;
+        },
+    },
+}
+
+</script>
